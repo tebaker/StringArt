eadef13f6ad982186c97160123bb1a83feb7958d
tebaker <taalonn@gmail.com>
2020-11-19 20:43:47 -0800

    Changed mr.jack black image to include grid lines for calibration. Completely working system for diviting the circle up into n equal parts and accuratly places dots at those points

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ class imgController:
     def __init__(self, img):
         # Grabbing image width, height
         self.w, self.h = img.size
+        self.xCenter = np.floor(self.w / 2)
+        self.yCenter = np.floor(self.h / 2)
         
         # Creating image matrix, filling all with False (eg white pixels) 
         self.imgMatrix = [[False for i in range(self.w)] for j in range(self.h)]
@@ -15,7 +17,7 @@ class imgController:
 
         # Scribing circle based on short side of image
         self.scribeCircle(img, self.h)
-        self.scribeNails(img, self.h)
+        self.scribeNails(img, self.h, 8)
         img.show()
 
         # Writing img data into img matrix
@@ -29,24 +31,36 @@ class imgController:
 
         # Drawing circle matching short side boarders of image
         # first argument = (upperX, upperY, lowerX, lowerY). circle will be drawn in that bounding box 
-        draw.ellipse((0, 0, shortSide, shortSide), outline="green", width=2)
+        draw.ellipse((0, 0, shortSide, shortSide), outline="green", width=3)
         del draw
 
-    def scribeNails(self, img, shortSide):
+    def scribeNails(self, img, shortSide, numNails):
         draw = ImageDraw.Draw(img)
+        # Dotting the middle for reference
+        draw.ellipse((shortSide/2-10,shortSide/2-10,shortSide/2+10,shortSide/2+10), fill="blue", outline="blue", width=3)
         # Drawing 100 'nails' on the image
-        for i in range(100):
-            degree = round(i / 100 * 360)
+        for i in range(numNails):
+            degree = round(i / numNails * 360)
 
-            xCenter = round(shortSide / 2)
-            yCenter = round(shortSide / 2)
+            # Converting deg to rad
+            x = np.cos(degree*np.pi/180)
+            y = np.sin(degree*np.pi/180)
 
-            xOff = round(np.cos(degree) * xCenter) + xCenter
-            yOff = round(np.sin(degree) * yCenter) + yCenter
+            # Calculating x offset from cartesian plane to image grid plane
+            xOffset = self.xCenter + self.xCenter * x
+            
+            # Calculating y offset requires flipping the sign of sin(theta) because
+            # the point (0, 0) starts at the upper left and (n, n) ends at the lower right
+            if y < 0:
+                yOffset = self.yCenter + np.abs(self.yCenter * y)
+            else:
+                yOffset = self.yCenter - (self.yCenter * y)
 
-            draw.ellipse((xOff, yOff, xOff + 5, yOff + 5), outline="green", width=2)
+            draw.ellipse((xOffset-10, yOffset-10, xOffset+10, yOffset+10), fill="blue", outline="blue", width=3)
 
-        print(degree, (xOff, yOff))
+            print(degree, x, y)
+
+        # print(degree, (xOff, yOff))
 
         del draw
 
