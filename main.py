45298cf430235decf8f3de56a8f7bd5b5bd1c731
tebaker <taalonn@gmail.com>
2020-11-19 21:39:57 -0800

    rewrote draw circle method to make it more general

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ import numpy as np
 
 class imgController:
     # Init will take in an image and prepare it for string processing
-    def __init__(self, img):
-        # Grabbing image width, height
+    def __init__(self, img, nails):
+        # Width, height of the image
         self.w, self.h = img.size
+
+        # Center of the image
         self.xCenter = np.floor(self.w / 2)
         self.yCenter = np.floor(self.h / 2)
         
+        self.radius = 0
+
+        if self.xCenter >= self.yCenter:
+            self.radius = self.xCenter
+        else:
+            self.radius = self.yCenter
+
         # Creating image matrix, filling all with False (eg white pixels) 
         self.imgMatrix = [[False for i in range(self.w)] for j in range(self.h)]
 
         # Converting image to black and white and adding dithering
-        # imgBW = img.convert('1')
+        # img = img.convert('1')
 
-        # Scribing circle based on short side of image
-        self.scribeCircle(img, self.h)
-        self.scribeNails(img, self.h, 8)
+        # Scribing circle from the center of the image to the edge
+        self.drawCircle(img, self.xCenter, self.yCenter, self.radius, "green", 5)
+        self.drawCircle(img, self.xCenter, self.yCenter, 10, "blue", 5)
+
+        # Placing a set amount of 'nails' around the image, following the line of the circle
+        self.scribeNails(img, self.h, nails)
         img.show()
 
         # Writing img data into img matrix
@@ -25,22 +37,24 @@ class imgController:
 
         # img.show()
 
-    def scribeCircle(self, img, shortSide):
+    # Draws a circle at center (x, y) with radius r
+    def drawCircle(self, img, x, y, r, outlineColor, lineWidth):
         # Drawing a circle on the image from the center out
         draw = ImageDraw.Draw(img)
 
         # Drawing circle matching short side boarders of image
         # first argument = (upperX, upperY, lowerX, lowerY). circle will be drawn in that bounding box 
-        draw.ellipse((0, 0, shortSide, shortSide), outline="green", width=3)
+        draw.ellipse((x - r, y - r, x + r, y + r), outline=outlineColor, width=lineWidth)
+
+        # Drawing circle to image
         del draw
 
     def scribeNails(self, img, shortSide, numNails):
         draw = ImageDraw.Draw(img)
-        # Dotting the middle for reference
-        draw.ellipse((shortSide/2-10,shortSide/2-10,shortSide/2+10,shortSide/2+10), fill="blue", outline="blue", width=3)
-        # Drawing 100 'nails' on the image
+
+        # Drawing 'nails' on the image
         for i in range(numNails):
-            degree = round(i / numNails * 360)
+            degree = (i / numNails * 360)
 
             # Converting deg to rad
             x = np.cos(degree*np.pi/180)
@@ -56,12 +70,9 @@ class imgController:
             else:
                 yOffset = self.yCenter - (self.yCenter * y)
 
-            draw.ellipse((xOffset-10, yOffset-10, xOffset+10, yOffset+10), fill="blue", outline="blue", width=3)
+            self.drawCircle(img, xOffset, yOffset, 10, "blue", 5)
 
-            print(degree, x, y)
-
-        # print(degree, (xOff, yOff))
-
+            # print(degree, xOffset, yOffset)
         del draw
 
 
@@ -86,7 +97,7 @@ def main():
     img = Image.open(path)
 
     # Passing image, 
-    imgController(img)
+    imgController(img, 8)
         
 
 
